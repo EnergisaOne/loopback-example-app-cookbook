822c95de26095eef04290962b2d3838ebf21358b
Ryan Graham <ryan@strongloop.com>
2014-01-22 13:17:12 -0800

    Initial commit
    Generated using berks --foodcritic

---
+++ README.md
@@ -0,0 +1,13 @@
+# sls-sample-app cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: StrongLoop, Inc. (<callback@strongloop.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'sls-sample-app'
+maintainer       'StrongLoop, Inc.'
+maintainer_email 'callback@strongloop.com'
+license          'MIT'
+description      'Deploys sls-sample-app'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
